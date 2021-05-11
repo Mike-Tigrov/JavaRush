0910c0b3a14e40350555143b6eb9f9ff8a0b999d
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-11 22:44:45 +0300

    Добавлена 1 задача

---
+++ src/JR_task_0_8_Comments_are_superfluous.java
@@ -0,0 +1,19 @@
+/*
+Комментарии излишни
+*/
+
+public class JR_task_0_8_Comments_are_superfluous {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 12;
+
+        // y = x * y;
+        y = x + y;
+
+        x = y - x;
+        y = y - x;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
