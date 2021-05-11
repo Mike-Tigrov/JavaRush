a05abe33ce86b0c3a05bb11bb5b55ec66858d0d1
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-11 22:00:44 +0300

    Первая задача

---
+++ src/JavaRush_task_0_1.java
@@ -0,0 +1,11 @@
+/*
+Амиго очень умный
+*/
+
+public class JavaRush_task_0_1 {
+
+            public static void main(String[] args) {
+            System.out.println("Амиго очень умный");//напишите тут ваш код
+        }
+    }
+
