3e14557d5451344ce05cbc9eaac23f58abb280dd
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-13 08:22:43 +0300

    Переназван класс + 1 задача

--- src/JR_task_0_1.java
+++ src/JR_task_0_1.java
@@ -2,7 +2,7 @@
 Амиго очень умный
 */
 
-public class JavaRush_task_0_1 {
+public class JR_task_0_1 {
 
             public static void main(String[] args) {
             System.out.println("Амиго очень умный");//напишите тут ваш код
---
+++ src/JR_task_1_3_I_am_by_the_ad.java
@@ -0,0 +1,11 @@
+/*
+«Я по объявлению…»
+*/
+
+public class JR_task_1_3_I_am_by_the_ad {
+
+        public static void main(String[] args) {
+            int a = 1, b = 2;//напишите тут ваш код
+        }
+    }
+
