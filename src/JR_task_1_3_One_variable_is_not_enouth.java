3bba77707b84b59d7ec6d302c83511f4f82fc2df
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-15 03:12:31 +0300

    +1 задача

---
+++ src/JR_task_1_3_One_variable_is_not_enouth.java
@@ -0,0 +1,12 @@
+/*
+Одной переменной недостаточно
+*/
+
+public class JR_task_1_3_One_variable_is_not_enouth {
+
+        public static void main(String[] args) {
+            int a = 1, b = 2;
+            String s = "String";//напишите тут ваш код
+        }
+    }
+
