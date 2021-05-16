6eb3ac5471be4946d2a0e1547139e36e313eda38
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-16 12:51:54 +0300

    +2 задачи

---
+++ src/JR_task_1_3_Display_square_of_number.java
@@ -0,0 +1,14 @@
+/*
+Выводим квадрат числа
+*/
+
+public class JR_task_1_3_Display_square_of_number {
+
+        public static int number = 25;
+
+        public static void main(String[] args) {
+            int a = number * number;
+            System.out.println(a);//напишите тут ваш код
+        }
+    }
+
---
+++ src/JR_task_1_4_Learn_onse_use_anywhere.java
@@ -0,0 +1,21 @@
+/*
+JavaRush. Learn once - use anywhere
+*/
+
+public class JR_task_1_4_Learn_onse_use_anywhere {
+
+        public static void main(String[] args) {
+            String s = "JavaRush. Learn once - use anywhere";
+            System.out.println(s);
+            System.out.println(s);
+            System.out.println(s);
+            System.out.println(s);
+            System.out.println(s);
+            System.out.println(s);
+            System.out.println(s);
+            System.out.println(s);
+            System.out.println(s);
+            System.out.println(s);
+        }
+    }
+
