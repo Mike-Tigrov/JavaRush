54090901802c17e833e43dbbb9383ec94de3488b
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-17 23:16:19 +0300

    + 1 задача

---
+++ src/JR_task_1_4_Happy_New_Year.java
@@ -0,0 +1,20 @@
+/*
+Ну это… С новым годом!
+*/
+
+public class JR_task_1_4_Happy_New_Year {
+
+        public static void main(String[] args) {
+            // String s = "Merry";
+            // System.out.println("Christmas");
+            // System.out.println("Merry New");
+            // System.out.println("weekend");
+            System.out.print("Happy New");
+            // System.out.println("weekend");
+            // System.out.println(s);
+            // System.out.print("Merry New!");
+            System.out.print(" ");
+            System.out.println("Year");
+        }
+    }
+
