26ef2fc5d7447f8e8b19c812c596ed5cc4e7eef9
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-17 23:20:42 +0300

    + 1 задача

---
+++ src/JR_task_1_4_Some_more_fixes.java
@@ -0,0 +1,13 @@
+/*
+Еще немного исправлений
+*/
+
+public class JR_task_1_4_Some_more_fixes {
+
+        public static void main(String[] args) {
+            String string1 = "Coding";
+            String string2 = "in";
+            String string3 = "java";
+            System.out.println(string1 + " " + string2 + " " + string3);
+        }
+    }
