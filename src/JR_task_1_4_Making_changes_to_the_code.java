ffb553eadb23a2e79b5b70025913c507043c3edb
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-17 23:18:21 +0300

    + 1 задача

---
+++ src/JR_task_1_4_Making_changes_to_the_code.java
@@ -0,0 +1,13 @@
+/*
+Вносим изменения в код
+*/
+
+public class JR_task_1_4_Making_changes_to_the_code {
+
+
+        public static void main(String[] args) {
+            String name = "Amigo";
+            String text = "Hello, " + name + "!";
+            System.out.println(text);
+        }
+    }
