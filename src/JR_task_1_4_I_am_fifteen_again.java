72229f620e6e82084327a2bcd9c64b6378accd9f
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-19 15:43:52 +0300

    + 1 задача

---
+++ src/JR_task_1_4_I_am_fifteen_again.java
@@ -0,0 +1,13 @@
+/*
+Мне снова 15(!)
+*/
+
+public class JR_task_1_4_I_am_fifteen_again {
+
+        public static void main(String[] args) {
+            String name = "Amigo";
+            int age = 15;
+            int money = 500;
+            int weight = 70;
+        }
+    }
