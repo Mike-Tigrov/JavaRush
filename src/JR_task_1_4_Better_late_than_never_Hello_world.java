6af79c945e865ff501ba0863ed9311b5b3c005f4
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-20 21:07:38 +0300

    + 1 задача

---
+++ src/JR_task_1_4_Better_late_than_never_Hello_world.java
@@ -0,0 +1,10 @@
+/*
+Лучше поздно, чем никогда: Hello world!
+*/
+
+public class JR_task_1_4_Better_late_than_never_Hello_world {
+
+        public static void main(String[] args) {
+            System.out.println("Поцелуй мой блестящий металлический зад!");
+        }
+    }
