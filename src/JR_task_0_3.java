14171a51d1821ebc65f917dc94cb0e1d8264eb5c
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-21 20:18:09 +0300

    + 1 задача

--- src/JR_task_0_3.java
+++ src/JR_task_0_3.java
@@ -5,7 +5,7 @@
 public class JR_task_0_3 {
 
         public static void main(String[] args) {
-            System.out.print("Я думаю, быть программистом - это круто"); //напишите тут ваш код
+            System.out.print("Я думаю, быть программистом - это круто");
         }
     }
 
