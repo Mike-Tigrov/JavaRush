8594a05827aed2f04793a813a86ce133225c270c
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-21 20:07:05 +0300

    + 1 задача

--- src/JR_task_0_1.java
+++ src/JR_task_0_1.java
@@ -5,7 +5,7 @@
 public class JR_task_0_1 {
 
             public static void main(String[] args) {
-            System.out.println("Амиго очень умный");//напишите тут ваш код
+            System.out.println("Амиго очень умный");
         }
     }
 
