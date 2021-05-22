4081109ef362ee4bfab5251dbaa6ec7366b76144
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-22 23:45:54 +0300

    + fix some task

--- src/JR_task_0_4_Declaring_variables.java
+++ src/JR_task_0_4_Declaring_variables.java
@@ -6,7 +6,7 @@ public class JR_task_0_4_Declaring_variables {
     public static void main(String[] args) {
         String name;
         String city;
-        int age;//напишите тут ваш код
+        int age;
     }
 }
 
--- src/JR_task_0_8_Bugs_and_fitch.java
+++ src/JR_task_0_8_Bugs_and_fitch.java
@@ -6,7 +6,7 @@ public class JR_task_0_8_Bugs_and_fitch {
 
 
         public static void main(String[] args) {
-            System.out.println("Это не баг, это фича."); //напишите тут ваш код
+            System.out.println("Это не баг, это фича.");
         }
     }
 
