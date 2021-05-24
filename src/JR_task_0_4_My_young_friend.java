d94b0b84f44b9825c5c1cd2943a1a96035c66107
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-24 23:18:13 +0300

    + fix some task

--- src/JR_task_0_4_My_young_friend.java
+++ src/JR_task_0_4_My_young_friend.java
@@ -8,6 +8,6 @@ public class JR_task_0_4_My_young_friend {
         int currentYear = 3126;
         int friendAge = 8;
         int birthday = currentYear - friendAge;
-        System.out.println(birthday);//напишите тут ваш код
+        System.out.println(birthday);
     }
 }
