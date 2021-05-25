af1703af009ab7bbac9e1c8fdc413ce2a0218883
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-25 21:16:23 +0300

    + task

---
+++ src/JR_task_1_7_Search_for_errors.java
@@ -0,0 +1,13 @@
+/*
+Поиск ошибок
+*/
+
+public class JR_task_1_7_Search_for_errors {
+
+        public static void main(String[] args) {
+            int a = 56;
+            int b = 30;
+            int c = 14;
+            System.out.print(a + b + c);
+        }
+    }
