f6fb4d861020a9cf9a16be2e10b8d28bc314e7e1
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-26 22:43:59 +0300

    + fix some task

--- src/JR_task_1_3_On_our_screen_a_variable.java
+++ src/JR_task_1_3_On_our_screen_a_variable.java
@@ -7,7 +7,7 @@ public class JR_task_1_3_On_our_screen_a_variable {
 
         public static void main(String[] args) {
             String name = "1+2+3";
-            System.out.println(name); //напишите тут ваш код
+            System.out.println(name);
         }
     }
 
