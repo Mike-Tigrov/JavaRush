ee256cee16b6643ea8399514ad7286c8981a828d
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-26 22:42:09 +0300

    + task

--- src/JR_task_1_3_Display_square_of_number.java
+++ src/JR_task_1_3_Display_square_of_number.java
@@ -8,7 +8,7 @@ public class JR_task_1_3_Display_square_of_number {
 
         public static void main(String[] args) {
             int a = number * number;
-            System.out.println(a);//напишите тут ваш код
+            System.out.println(a);
         }
     }
 
