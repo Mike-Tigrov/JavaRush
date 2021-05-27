ad867f7a39983282710a5d5e341b6d0e64fdffa3
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-27 19:28:45 +0300

    + task

---
+++ src/JR_task_1_8_Number_squared.java
@@ -0,0 +1,15 @@
+/*
+Число в квадрате
+*/
+
+public class JR_task_1_8_Number_squared {
+
+        public static void main(String[] args) {
+            int a = sqr(5);
+            System.out.println(a);//напишите тут ваш код
+        }
+
+        public static int sqr(int a) {
+            return a * a;
+        }
+    }
