c4a1832ce0cd7dde8359062dd9172773e6285e68
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-29 00:18:10 +0300

    + task

---
+++ src/JR_task_7_9_Check_orderliness.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class JR_task_7_9_Check_orderliness {
+
+/*
+Проверка на упорядоченность
+
+Для вставки:
+дедушка
+бабушка
+папа
+мама
+сын
+дочь
+коты
+собака
+программа
+машина
+*/
+
+        public static void main(String[] args) throws IOException {
+            //напишите тут ваш код
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            ArrayList<String> list = new ArrayList<String>();
+
+            for (int i = 0; i < 10; i++) {
+                list.add(i, reader.readLine());
+            }
+
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i).length() >= list.get(i+1).length()) {
+                    System.out.print(i + 1);
+                    break;
+                }
+            }
+
+        }
+}
