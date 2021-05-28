86f9434264102afbf7e6b6f40b840d24da704327
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-28 23:46:43 +0300

    + task

---
+++ src/JR_task_1_8_Choose_healthy_food_choose_fruit.java
@@ -0,0 +1,16 @@
+/*
+Выбирай здоровую еду! Выбирай фрукты!
+*/
+
+public class JR_task_1_8_Choose_healthy_food_choose_fruit {
+
+        public static void main(String[] args) {
+            String apple = "Яблоко";
+            String porridge = "Каша";
+            String sausage = "Колбаса";
+            String peach = "Персик";
+            System.out.println(apple);
+            System.out.println(peach);
+            //напишите тут ваш код
+        }
+    }
