7e36cbed43ca3bb470237318900c77940f5adef1
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-28 23:48:56 +0300

    + task

---
+++ src/JR_1_8_Favorite_poem.java
@@ -0,0 +1,15 @@
+public class JR_1_8_Favorite_poem {
+/*
+Любимое стихотворение
+*/
+
+        public static void main(String[] args) {
+            System.out.println("Мое любимое стихотворение:");
+            System.out.println();
+            System.out.println("По вечерам над ресторанами,");
+            System.out.println("Горячий воздух дик и глух.");
+            System.out.println("И правит окриками пьяными,");
+            System.out.println("Весенний и тлетворный дух.");
+
+        }
+    }
