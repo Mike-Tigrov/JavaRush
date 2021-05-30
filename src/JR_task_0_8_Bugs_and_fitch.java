e58f6cec999b955154fd88dfd9e1741b3d666b25
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-30 23:09:12 +0300

    + fix some task

--- src/JR_task_0_8_Bugs_and_fitch.java
+++ src/JR_task_0_8_Bugs_and_fitch.java
@@ -4,7 +4,6 @@
 
 public class JR_task_0_8_Bugs_and_fitch {
 
-
         public static void main(String[] args) {
             System.out.println("Это не баг, это фича.");
         }
--- src/JR_task_1_3_On_our_screen_a_variable.java
+++ src/JR_task_1_3_On_our_screen_a_variable.java
@@ -4,7 +4,6 @@
 
 public class JR_task_1_3_On_our_screen_a_variable {
 
-
         public static void main(String[] args) {
             String name = "1+2+3";
             System.out.println(name);
