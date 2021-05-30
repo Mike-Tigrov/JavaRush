56df7e66eb510ad28ee87c38b4c3ac375d89f3ab
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-30 23:10:31 +0300

    + fix some task

--- src/JR_task_1_4_Making_changes_to_the_code.java
+++ src/JR_task_1_4_Making_changes_to_the_code.java
@@ -4,7 +4,6 @@
 
 public class JR_task_1_4_Making_changes_to_the_code {
 
-
         public static void main(String[] args) {
             String name = "Amigo";
             String text = "Hello, " + name + "!";
--- src/JR_task_1_8_Choose_healthy_food_choose_fruit.java
+++ src/JR_task_1_8_Choose_healthy_food_choose_fruit.java
@@ -11,6 +11,5 @@ public class JR_task_1_8_Choose_healthy_food_choose_fruit {
             String peach = "Персик";
             System.out.println(apple);
             System.out.println(peach);
-            //напишите тут ваш код
-        }
+                    }
     }
