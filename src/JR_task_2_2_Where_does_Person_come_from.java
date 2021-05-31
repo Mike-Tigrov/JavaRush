0db2670778496108630f24561566e9157076f417
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-05-31 23:40:09 +0300

    + new task

---
+++ src/JR_task_2_2_Where_does_Person_come_from.java
@@ -0,0 +1,16 @@
+public class JR_task_2_2_Where_does_Person_come_from {
+    /*
+Откуда берутся Person?
+*/
+
+        public static void main(String[] args) {
+            Person person = new Person();//напишите тут ваш код
+        }
+
+        public static class Person {
+            int age;
+            int money;
+            String name;
+            int weight;//напишите тут ваш код
+        }
+    }
