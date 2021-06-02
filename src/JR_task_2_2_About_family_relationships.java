5ba9176b10cdc89f3ef3049ddc84d78a1626d9f6
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-02 23:37:10 +0300

    + task

---
+++ src/JR_task_2_2_About_family_relationships.java
@@ -0,0 +1,24 @@
+public class JR_task_2_2_About_family_relationships {
+    /*
+О семейных отношениях
+*/
+
+        public static void main(String[] args) {
+            Man man = new Man();
+            Woman woman = new Woman();
+            woman.husband = man;
+            man.wife = woman; //напишите тут ваш код
+        }
+
+        public static class Man {
+            public int age;
+            public int height;
+            public Woman wife;
+        }
+
+        public static class Woman {
+            public int age;
+            public int height;
+            public Man husband;
+        }
+    }
