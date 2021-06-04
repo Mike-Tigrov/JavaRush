6dc099015d7005b45f5a8b48b824968f2e6b4c4d
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-04 21:44:14 +0300

    + fix task

--- src/JR_task_7_9_Play_in_Jolushka.java
+++ src/JR_task_7_9_Play_in_Jolushka.java
@@ -30,53 +30,57 @@ public class JR_task_7_9_Play_in_Jolushka {
 150
 */
 
-          public static void main(String[] args) throws Exception {
-            //напишите тут ваш код
-            ArrayList<Integer> main = new ArrayList<Integer>();
-            ArrayList<Integer> na3 = new ArrayList<Integer>();
-            ArrayList<Integer> na2 = new ArrayList<Integer>();
-            ArrayList<Integer> last = new ArrayList<Integer>();
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        ArrayList<Integer> main = new ArrayList<Integer>();
+        ArrayList<Integer> na3 = new ArrayList<Integer>();
+        ArrayList<Integer> na2 = new ArrayList<Integer>();
+        ArrayList<Integer> last = new ArrayList<Integer>();
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-            for (int i = 0; i < 20; i++) {
-                main.add(i, Integer.parseInt(reader.readLine()));
+
+        for (int i = 0; i < 20; i++) {
+            main.add(i, Integer.parseInt(reader.readLine()));
+        }
+
+        for (int i = 0; i < main.size(); i++) {
+            if (main.get(i) % 3 == 0) {
+                na3.add(main.get(i));
+            } else if (main.get(i) % 2 == 0) {
+                na2.add(main.get(i));
+            } else {
+                last.add(main.get(i));
+            }
+        }
+            for (int i = 0; i < main.size(); i++) {
+                if (main.get(i) % 3 == 0 & main.get(i) % 2 == 0) {
+                    na3.add(main.get(i));
+                    na2.add(main.get(i));
+                }
             }
 
+
+            for (Integer main_print : main) {
+                System.out.println(main_print);
+            }
+
+            System.out.println("Делятся на 3:");
+
+            for (Integer na3_print : na3) {
+                System.out.println(na3_print);
+            }
+
+            System.out.println("Делятся на 2:");
+
+            for (Integer na2_print : na2) {
+                System.out.println(na2_print);
+            }
+
+            System.out.println("Остальные:");
         }
-        public static void printList(ArrayList<Integer> list) {
+        public static void printList (ArrayList < Integer > list) {
             //напишите тут ваш код
+
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+    }
