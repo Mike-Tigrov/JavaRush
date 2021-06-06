a6239d3dd718947fb86275e18a317ac7cd7c082d
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-06 20:50:36 +0300

    + fix task

--- src/Java_in_day.java
+++ src/Java_in_day.java
@@ -3,6 +3,6 @@ import java.util.Date;
 
 public class Java_in_day {
     public static void main(String[] args) {
-        System.out.println(05/06/2021);
+        System.out.println("05/06/2021");
     }
 }
