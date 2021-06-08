eb98fb540fbe9a117b69364b090d1a41f4a146e2
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-08 20:44:25 +0300

    + task

---
+++ src/JR_task_2_2_Salary_increase.java
@@ -0,0 +1,14 @@
+public class JR_task_2_2_Salary_increase {
+    /*
+Прибавка к зарплате
+*/
+
+    public static void main(String[] args) {
+            hackSalary(700);        //зарплата
+        }
+
+        public static void hackSalary(int salary) {
+
+            System.out.print("Твоя зарплата составляет: " + (salary + 100) + " долларов в месяц.");
+        }
+    }
