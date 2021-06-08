92d86610696e33d7040a47b9fe4eb6f9670c67c8
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-08 20:46:11 +0300

    + task

---
+++ src/JR_task_2_2_Piece_of_calculator.java
@@ -0,0 +1,12 @@
+public class JR_task_2_2_Piece_of_calculator {
+    /*
+Кусочек калькулятора
+*/
+
+           public static void main(String[] args) {
+            int a = 2;
+            int b = 5;
+            System.out.println(a+b);
+            System.out.println(a*b);//напишите тут ваш код
+        }
+    }
