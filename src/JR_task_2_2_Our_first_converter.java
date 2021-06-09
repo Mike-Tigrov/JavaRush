0c2405424815da9c32785ba26d1c76be7810c12a
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-09 20:24:40 +0300

    + task

---
+++ src/JR_task_2_2_Our_first_converter.java
@@ -0,0 +1,15 @@
+public class JR_task_2_2_Our_first_converter {
+    /*
+Наш первый конвертер!
+*/
+
+          public static void main(String[] args) {
+            System.out.println(convertCelsiusToFahrenheit(60));
+        }
+
+        public static double convertCelsiusToFahrenheit(int celsius) {
+            double fahrenheit = celsius * (9.0 / 5.0) + 32;
+
+            return fahrenheit;
+        }
+    }
