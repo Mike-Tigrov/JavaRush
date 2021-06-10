d1fa387d1489a6e79f68ccde1758bbd2c5255a79
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-10 20:23:22 +0300

    + fix task

--- src/JR_task_2_2_Count_circumference.java
+++ src/JR_task_2_2_Count_circumference.java
@@ -9,6 +9,6 @@ public class JR_task_2_2_Count_circumference {
 
         public static void printCircleLength(int radius) {
             double Pi = 3.14;
-            System.out.println(2 * Pi * radius);//напишите тут ваш код
+            System.out.println(2 * Pi * radius);
         }
     }
