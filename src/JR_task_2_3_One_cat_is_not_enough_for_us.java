01d9b88393a3d2b43406e1c989ad58a935f1be7b
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-10 21:20:35 +0300

    + fix task

--- src/JR_task_2_3_One_cat_is_not_enough_for_us.java
+++ src/JR_task_2_3_One_cat_is_not_enough_for_us.java
@@ -1,2 +1,14 @@
 public class JR_task_2_3_One_cat_is_not_enough_for_us {
-}
+    /*
+Одного кота нам мало
+*/
+
+        public static void main(String[] args) {
+            Cat a = new Cat();
+            Cat b = new Cat();    //напишите тут ваш код
+        }
+
+        public static class Cat {
+
+        }
+    }
