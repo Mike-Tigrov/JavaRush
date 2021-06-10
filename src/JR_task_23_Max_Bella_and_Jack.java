8bb3ff374d5d78c1bfbe92f0824dc9eddc03fbc5
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-10 22:21:05 +0300

    + task

--- src/JR_task_23_Max_Bella_and_Jack.java
+++ src/JR_task_23_Max_Bella_and_Jack.java
@@ -1,2 +1,21 @@
 public class JR_task_23_Max_Bella_and_Jack {
-}
+    /*
+Макс, Белла и Джек
+*/
+
+         public static void main(String[] args) {
+            Dog dog1 = new Dog();
+            dog1.name = "Max";
+
+            Dog dog2 = new Dog();
+            dog2.name = "Bella";
+            Dog dog3 = new Dog();
+            dog3.name = "Jack";
+
+            //напишите тут ваш код
+        }
+
+        public static class Dog {
+            public String name;
+        }
+    }
