d14a0b872489c400a87124cb23b828efde24f35e
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-11 22:44:30 +0300

    + task

---
+++ src/JR_task_2_3_Biased_reality.java
@@ -0,0 +1,9 @@
+public class JR_task_2_3_Biased_reality {
+    /*
+Необъективная реальность
+*/
+
+           public static void main(String[] args) {
+            System.out.println("Будешь плохо кодить, придет Java и съест твою память");//напишите тут ваш код
+        }
+    }
