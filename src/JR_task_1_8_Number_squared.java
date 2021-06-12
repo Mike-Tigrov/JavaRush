c371b497dd60db28e20c208a772599806c7123e0
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-12 22:38:27 +0300

    + fix task

--- src/JR_task_1_8_Number_squared.java
+++ src/JR_task_1_8_Number_squared.java
@@ -6,7 +6,7 @@ public class JR_task_1_8_Number_squared {
 
         public static void main(String[] args) {
             int a = sqr(5);
-            System.out.println(a);//напишите тут ваш код
+            System.out.println(a);
         }
 
         public static int sqr(int a) {
