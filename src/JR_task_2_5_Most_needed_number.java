afa3ec725cc4cdd7512f75b77e9b4bcd46e4c641
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-13 21:44:18 +0300

    + task

---
+++ src/JR_task_2_5_Most_needed_number.java
@@ -0,0 +1,19 @@
+public class JR_task_2_5_Most_needed_number {
+
+    /*
+Самое нужное число
+*/
+
+        public static void main(String[] args) {
+            int x = 1;
+            int y = 0;
+
+            y = y + 3 * x;
+            //  x = x * 2;
+            x = x * 16;
+            //  y = y + 2 * x;
+            y = y + x;
+
+            System.out.println(y);
+        }
+    }
