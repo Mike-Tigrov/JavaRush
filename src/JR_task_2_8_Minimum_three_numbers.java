edce1f0073de5991247b50bc3c1a6d68e5585fb9
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-15 22:13:38 +0300

    + task

---
+++ src/JR_task_2_8_Minimum_three_numbers.java
@@ -0,0 +1,18 @@
+public class JR_task_2_8_Minimum_three_numbers {
+    /*
+Минимум трёх чисел
+*/
+
+// Не решена!
+        public static int min(int a, int b, int c) {
+            //напишите тут ваш код
+            return;
+        }
+
+        public static void main(String[] args) {
+            System.out.println(min(1, 2, 3));
+            System.out.println(min(-1, -2, -3));
+            System.out.println(min(3, 5, 3));
+            System.out.println(min(5, 5, 10));
+        }
+}
