12495ceba1ba68c63a4d46bf29602eb0d6ae2247
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-16 21:25:14 +0300

    + fix task

--- src/JR_task_2_8_NOT_COMPLITE_Minimum_three_numbers.java
+++ src/JR_task_2_8_NOT_COMPLITE_Minimum_three_numbers.java
@@ -6,8 +6,15 @@ public class JR_task_2_8_NOT_COMPLITE_Minimum_three_numbers {
 // Не решена!
         public static int min(int a, int b, int c) {
             //напишите тут ваш код
-            return;
+            if (a < b && a < c) {
+                return a;
+            } else if (b < a && b < c) {
+                return b;
+            } else if (c < a && c < b) {
+                return c;
+            }
         }
+
 
         public static void main(String[] args) {
             System.out.println(min(1, 2, 3));
