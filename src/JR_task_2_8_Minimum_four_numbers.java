c5cf3dce05c65529d193eebcc35fc1bcce222363
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-18 23:26:47 +0300

    + task

---
+++ src/JR_task_2_8_Minimum_four_numbers.java
@@ -0,0 +1,33 @@
+public class JR_task_2_8_Minimum_four_numbers {
+    /*
+Минимум четырех чисел
+*/
+
+    public class Solution {
+        public static int min(int a, int b, int c, int d) {
+            int m4ab = min(a,b);
+            int m4cd = min(c,d);
+            int m4abcd = min(m4ab,m4cd);
+            return m4abcd;
+
+            //напишите тут ваш код
+
+        }
+
+        public static int min(int a, int b) {
+            int m2;
+            if (a<b)
+                m2 = a;
+            else m2 = b;
+            return m2;//напишите тут ваш код
+
+        }
+
+        public static void main(String[] args) throws Exception {
+            System.out.println(min(-20, -10));
+            System.out.println(min(-40, -10, -30, 40));
+            System.out.println(min(-20, -40, -30, 40));
+            System.out.println(min(-20, -10, -40, 40));
+            System.out.println(min(-20, -10, -30, -40));
+        }
+    }
