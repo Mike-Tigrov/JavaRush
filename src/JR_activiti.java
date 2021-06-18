1d77bfe8afccea4162438668ffdb4840d2752d3b
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-18 23:23:18 +0300

    + fix task

--- src/JR_activiti.java
+++ src/JR_activiti.java
@@ -1,6 +1,6 @@
 public class JR_activiti {
     public static void main (String[] agrs) {
-        byte number = (byte) (1 + Math.random() * 3);
+        byte number = (byte) (1 + Math.random() * 9);
         System.out.print(number);
     }
 }
