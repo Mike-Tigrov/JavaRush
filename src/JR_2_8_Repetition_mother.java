d8aac8a6ef4199a64408e3b3ebba8715826dd164
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-18 23:30:09 +0300

    + task

---
+++ src/JR_2_8_Repetition_mother.java
@@ -0,0 +1,16 @@
+public class JR_2_8_Repetition_mother {
+    /*
+Повторенье-мать
+*/
+
+    public class Solution {
+        public static void print3(String text) {
+            System.out.println(text+"\n"+text+"\n"+text);//напишите тут ваш код
+
+        }
+
+        public static void main(String[] args) {
+            print3("I love you!");
+        }
+    }
+}
