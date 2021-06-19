01f3e5e664071f4be0cbde0f4444d2e60baab26e
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-19 21:48:18 +0300

    + fix task

---
+++ src/JR_task_2_8_Repetition_mother.java
@@ -0,0 +1,15 @@
+public class JR_task_2_8_Repetition_mother {
+    /*
+Повторенье-мать
+*/
+
+        public static void print3(String text) {
+            System.out.println(text+"\n"+text+"\n"+text);
+        }
+
+        public static void main(String[] args)
+        {
+            print3("I love you!");
+        }
+    }
+
