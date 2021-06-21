486ed257240e047d1ac0f2e46fd9615164287824
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-21 18:42:08 +0300

    + fix task

--- src/JR_task_7_12_Family_census.java
+++ src/JR_task_7_12_Family_census.java
@@ -12,11 +12,21 @@ public class JR_task_7_12_Family_census {
 
         // Создаём бабушку Марусю
         Human Grandmother_Marysya = new Human();
-        Human Mother = new Human();
-        Human Father = new Human();
-        Human Sun_Max = new Human();
-        Human Sun_Vlad = new Human();
-        Human Doughter = new Human();
+
+        // Создаём маму Вику, её папа - Вася, мама - Маша
+        Human Vika = new Human();
+
+        // Создаём папу Сергея, его папа - Петя, мама - Маруся
+        Human Sergey = new Human();
+
+        // Создаём сына Максима, его папа - Сергей, мама - Вика
+        Human Max = new Human();
+
+        // Создаём сына Влада, его папа - Сергей, мама - Вика
+        Human Vlad = new Human();
+
+        // Создаём дочь Ольгу, её папа Сергей, мама Вика
+        Human Olga = new Human();
     }
 // Создаём класс Человек с полями: имя, пол, возраст, наличие отца, матери:
     public static class Human {
