a73cdc20b4fbe20c3f68215903d6cf1b7d1607c9
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-22 20:40:08 +0300

    + fix

--- src/JR_task_7_12_Family_census.java
+++ src/JR_task_7_12_Family_census.java
@@ -1,32 +1,43 @@
+import java.util.ArrayList;
+
 public class JR_task_7_12_Family_census {
     public static void main(String[] args) {
 
+        // Выводим всех на экран
+        System.out.println();
+
         // Создаём дедушку Васю
-        Human Vasya = new Human();
+        Human Vasya = new Human("Вася", true, 73);
 
         // Создаём дедушку Петю
-        Human Petya = new Human();
+        Human Petya = new Human("Петя", true, 78);
 
         // Создаём бабушку Машу
-        Human Masha = new Human();
+        Human Masha = new Human("Маша", false, 69);
 
         // Создаём бабушку Марусю
-        Human Marysya = new Human();
+        Human Marysya = new Human("Маруся", false, 67);
 
         // Создаём маму Вику, её папа - Вася, мама - Маша
-        Human Vika = new Human();
+        Human Vika = new Human("Вика", false, 41, Vasya.father, Masha.mother);
 
         // Создаём папу Сергея, его папа - Петя, мама - Маруся
-        Human Sergey = new Human();
+        Human Sergey = new Human("Сергей", true, 42, Petya.father, Marysya.mother);
 
         // Создаём сына Максима, его папа - Сергей, мама - Вика
-        Human Max = new Human();
+        Human Max = new Human("Максим", true, 17, Sergey.father, Vika.mother);
 
         // Создаём сына Влада, его папа - Сергей, мама - Вика
-        Human Vlad = new Human();
+        Human Vlad = new Human("Владислав", true, 14, Sergey.father, Vika.mother);
 
         // Создаём дочь Ольгу, её папа Сергей, мама Вика
-        Human Olga = new Human();
+        Human Olga = new Human("Ольга", false, 11, Sergey.father, Vika.mother);
+
+        ArrayList<Human> Nine_Humans = new ArrayList<Human>();
+        Nine_Humans.add(Vasya);
+        Nine_Humans.add(Petya);
+
+        for (int i = 0; i < )
     }
 // Создаём класс Человек с полями: имя, пол, возраст, наличие отца, матери:
     public static class Human {
@@ -51,7 +62,7 @@ this.age = age;
 this.father = father;
 this.mother = mother;
         }
-
+@Override
         public String toString() {
             String text = "";
             text += "Имя: " + this.name;
