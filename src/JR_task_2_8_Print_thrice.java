243e523c4ca303041f4ae76f16ca943b99983fbe
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-23 20:15:46 +0300

    + task

---
+++ src/JR_task_2_8_Print_thrice.java
@@ -0,0 +1,16 @@
+public class JR_task_2_8_Print_thrice {
+    /*
+Печатаем трижды
+*/
+
+    public class Solution {
+        public static void print3(String text) {
+            System.out.print(text+" "+text+" "+text);//напишите тут ваш код
+
+        }
+
+        public static void main(String[] args) {
+            print3("window");
+            print3("file");
+        }
+    }
