d107ef611cb4d59cd7ece74375b927fe477f3a0f
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-23 20:20:26 +0300

    + fix task

--- src/JR_task_2_8_Yes_even_to_the_Moon.java
+++ src/JR_task_2_8_Yes_even_to_the_Moon.java
@@ -1,3 +1,14 @@
 public class JR_task_2_8_Yes_even_to_the_Moon {
+/*
+Да хоть на Луну!
+*/
 
-}
+    public class Solution {
+        public static void main(String[] args) {
+            System.out.println(getWeight(888));
+        }
+
+        public static double getWeight(int weightEarth) {
+            return weightEarth * 0.17;//напишите тут ваш код
+        }
+    }
