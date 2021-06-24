1fbf5cc7b14b7b6caacca901bea2b8edcc269c85
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-24 20:06:15 +0300

    + task

---
+++ src/JR_3_3_task_Immidiately_to_print.java
@@ -0,0 +1,13 @@
+public class JR_3_3_task_Immidiately_to_print {
+    /*
+Немедленно в печать
+*/
+
+        public static void printString(String s){
+
+            System.out.println(s);// напишите тут ваш код
+        }
+        public static void main(String[] args) {
+            printString("Hello, Amigo!");
+        }
+    }
