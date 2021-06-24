7cdef9dbf272c232dc8863ffb05c88d367b13e63
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-24 20:04:40 +0300

    + fix

--- src/JR_task_3_3_Currency_exchange.java
+++ src/JR_task_3_3_Currency_exchange.java
@@ -1,19 +1,16 @@
-public class JR_3_3_task_Currency_exchange {
+public class JR_task_3_3_Currency_exchange {
 
     /*
 Обмен валют
 */
 
         public static void main(String[] args) {
-
             System.out.println(convertEurToUsd(2, 3.1));
             System.out.println(convertEurToUsd(4, 2.2));
-
         }
 
         public static double convertEurToUsd(int eur, double exchangeRate) {
             {
-
                 return eur*exchangeRate;
             }
         }
