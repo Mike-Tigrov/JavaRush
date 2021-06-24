d6d28a4d5983f8833b202825dce73ce00adf03c4
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-24 20:07:13 +0300

    + fix

--- src/JR_3_3_task_Immidiately_to_print.java
+++ src/JR_3_3_task_Immidiately_to_print.java
@@ -1,13 +1,14 @@
 public class JR_3_3_task_Immidiately_to_print {
-    /*
+/*
 Немедленно в печать
 */
 
-        public static void printString(String s){
-
-            System.out.println(s);// напишите тут ваш код
+        public static void printString(String s)
+        {
+            System.out.println(s);
         }
-        public static void main(String[] args) {
+        public static void main(String[] args)
+        {
             printString("Hello, Amigo!");
         }
     }
