d46870a31fa9ade8b652804d74a8eb8182c68bd7
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-26 21:08:03 +0300

    + task

---
+++ src/JR_3_4_task_I_was_born_so_long_ago.java
@@ -0,0 +1,13 @@
+public class JR_3_4_task_I_was_born_so_long_ago {
+
+/*
+Я так давно родился
+*/
+
+    public class Solution {
+        public static void main(String[] args) {
+
+            System.out.println("JULY"+" "+2+" "+1985); //напишите тут ваш код
+
+        }
+    }
