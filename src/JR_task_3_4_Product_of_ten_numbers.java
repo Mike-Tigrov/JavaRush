e28b8ff6de486d6eaf81e6598b0fb4034374bfb8
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-27 22:17:42 +0300

    + fix

---
+++ src/JR_task_3_4_Product_of_ten_numbers.java
@@ -0,0 +1,10 @@
+public class JR_task_3_4_Product_of_ten_numbers {
+
+/*
+Произведение 10 чисел
+*/
+
+        public static void main(String[] args) {
+            System.out.println(1*2*3*4*5*6*7*8*9*10);
+        }
+    }
