d7c5706d033d0b5dca5c92c7deb84f1a6a7278c7
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-28 18:24:25 +0300

    + task

---
+++ src/JR_3_4_task_Puzzle_with_brackets.java
@@ -0,0 +1,11 @@
+public class JR_3_4_task_Puzzle_with_brackets {
+
+/*
+Головоломка со скобками
+*/
+
+
+        public static void main(String[] args) {
+            System.out.println(2 * (3 + 4 * (5 + 6 * 7)));
+        }
+    }
