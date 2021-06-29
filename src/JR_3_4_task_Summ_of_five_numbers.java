4f09da84e92c28bc62b1784f9b9561797d0c34dd
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-29 20:52:57 +0300

    + task

---
+++ src/JR_3_4_task_Summ_of_five_numbers.java
@@ -0,0 +1,21 @@
+public class JR_3_4_task_Summ_of_five_numbers {
+
+/*
+Сумма 5 чисел
+*/
+
+
+        public static void main(String[] args) {
+            int i = 1;
+            System.out.println(i);
+            i = i + 2;
+            System.out.println(i);
+            i = i + 3;
+            System.out.println(i);
+            i = i + 4;
+            System.out.println(i);
+            i = i + 5;
+            System.out.println(i);
+
+        }
+    }
