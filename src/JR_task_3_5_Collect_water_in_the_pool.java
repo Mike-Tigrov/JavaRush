359dec75b921fced6f2e2c8a2836ede03f491592
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-06-30 18:43:42 +0300

    + fix

---
+++ src/JR_task_3_5_Collect_water_in_the_pool.java
@@ -0,0 +1,16 @@
+public class JR_task_3_5_Collect_water_in_the_pool {
+
+/*
+Набираем воду в бассейн
+*/
+
+        public static void main(String[] args)
+        {
+            System.out.println(getVolume(25, 5, 2));
+        }
+
+        public static long getVolume(int a, int b, int c)
+        {
+            return a*b*c*1000;
+        }
+    }
