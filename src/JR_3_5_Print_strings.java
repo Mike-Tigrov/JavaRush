e8b226ee7cd4c4aef1ae246d2e58ec26d9cdcdff
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-01 21:24:01 +0300

    + task

---
+++ src/JR_3_5_Print_strings.java
@@ -0,0 +1,18 @@
+public class JR_3_5_Print_strings {
+
+/*
+Печатаем строки
+*/
+
+
+        public static void main(String[] args) {
+            writeToConsole("Hello world!");
+        }
+
+        public static void writeToConsole(String s) {
+
+            System.out.print("printing: ");
+            System.out.println(s);
+
+        }
+    }
