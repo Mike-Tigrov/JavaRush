ea5eb59fb531c2172a454dccff6bb3d8f6f4ca3d
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-01 19:22:39 +0300

    + fix task

---
+++ src/JR_3_5_Convert_time.java
@@ -0,0 +1,18 @@
+public class JR_3_5_Convert_time {
+
+/*
+Конвертируем время
+*/
+
+
+        public static int convertToSeconds(int hour){//напишите тут ваш код
+
+
+            return hour*60*60;
+
+        } public static void main(String[] args) {
+            System.out.println(convertToSeconds(24));
+            System.out.println(convertToSeconds(1)); //напишите тут ваш код
+
+        }
+    }
