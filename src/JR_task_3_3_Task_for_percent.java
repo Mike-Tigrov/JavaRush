6ebd6d60fd2c99963eaed2256f70d2e932f6408c
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-04 17:47:28 +0300

    Update and rename JR_3_3_task_Tast_for_percent.java to JR_task_3_3_Task_for_percent.java
    Rename + fix

---
+++ src/JR_task_3_3_Task_for_percent.java
@@ -0,0 +1,15 @@
+public class JR_task_3_3_Task_for_percent {
+        /*
+Задача на проценты
+*/
+
+        public static double addTenPercent(int i) 
+        {
+            return i * 1.1;
+        }
+
+        public static void main(String[] args) 
+        {
+            System.out.println(addTenPercent(5));
+        }
+    }
