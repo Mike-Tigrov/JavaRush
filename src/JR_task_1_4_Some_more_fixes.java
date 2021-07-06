616cabef6586720063e331da824a137b95d497aa
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-06 23:09:35 +0300

    Update JR_task_1_4_Some_more_fixes.java
    + fix

--- src/JR_task_1_4_Some_more_fixes.java
+++ src/JR_task_1_4_Some_more_fixes.java
@@ -2,12 +2,14 @@
 Еще немного исправлений
 */
 
-public class JR_task_1_4_Some_more_fixes {
+public class JR_task_1_4_Some_more_fixes 
+{
 
-        public static void main(String[] args) {
+        public static void main(String[] args) 
+        {
             String string1 = "Coding";
             String string2 = "in";
             String string3 = "java";
             System.out.println(string1 + " " + string2 + " " + string3);
         }
-    }
+}
