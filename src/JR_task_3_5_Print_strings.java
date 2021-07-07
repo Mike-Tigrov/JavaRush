ec2468af66584490964db92683cd725f2ed8f48b
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-07 23:12:01 +0300

    + some fix

--- src/JR_task_3_5_Print_strings.java
+++ src/JR_task_3_5_Print_strings.java
@@ -4,15 +4,15 @@ public class JR_task_3_5_Print_strings {
 Печатаем строки
 */
 
-        public static void main(String[] args) {
+        public static void main(String[] args)
+        {
             writeToConsole("Hello world!");
         }
 
-        public static void writeToConsole(String s) {
-
+        public static void writeToConsole(String s)
+        {
             System.out.print("printing: ");
             System.out.println(s);//напишите тут ваш код
-
         }
     }
 
