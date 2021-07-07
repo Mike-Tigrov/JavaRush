782452d402d15c1dcb765761fcd322dbcbf695e8
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-07 23:10:14 +0300

    + new task

--- src/JR_task_3_5_Print_strings.java
+++ src/JR_task_3_5_Print_strings.java
@@ -1,2 +1,20 @@
 public class JR_task_3_5_Print_strings {
+
+/*
+Печатаем строки
+*/
+
+    public class Solution {
+        public static void main(String[] args) {
+            writeToConsole("Hello world!");
+        }
+
+        public static void writeToConsole(String s) {
+
+            System.out.print("printing: ");
+            System.out.println(s);//напишите тут ваш код
+
+        }
+    }
+
 }
