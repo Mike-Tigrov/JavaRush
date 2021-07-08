275cd8d7467c6b3b4ca528504579e37880b5b93f
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-08 20:27:48 +0300

    + fix

--- src/JR_task_3_6_Mother_washed_the_frame.java
+++ src/JR_task_3_6_Mother_washed_the_frame.java
@@ -6,8 +6,14 @@ public class JR_task_3_6_Mother_washed_the_frame {
 
          public static void main(String[] args) {
             //напишите тут ваш код
-String A = "Мама";
-        String B;
-                String C;
+        String A = "Мама";
+        String B = "Мыла";
+        String C = "Раму";
+             System.out.println(A+B+C);
+             System.out.println(A+C+B);
+             System.out.println(B+A+C);
+             System.out.println(B+C+A);
+             System.out.println(C+A+B);
+             System.out.println(C+B+A);
         }
 }
