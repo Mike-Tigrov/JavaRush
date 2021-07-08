de1a15ad335bb77916d82644777c631619a390c9
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-08 20:22:31 +0300

    + task

--- src/JR_task_3_6_Mother_washed_the_frame.java
+++ src/JR_task_3_6_Mother_washed_the_frame.java
@@ -6,6 +6,8 @@ public class JR_task_3_6_Mother_washed_the_frame {
 
          public static void main(String[] args) {
             //напишите тут ваш код
-
+String A = "Мама";
+        String B;
+                String C;
         }
 }
