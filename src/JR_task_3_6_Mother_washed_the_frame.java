49cfb6ec8fb476c7482bfa5b0ac5321bc41270d7
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-08 20:21:15 +0300

    + fix

--- src/JR_task_3_6_Mother_washed_the_frame.java
+++ src/JR_task_3_6_Mother_washed_the_frame.java
@@ -1,3 +1,11 @@
 public class JR_task_3_6_Mother_washed_the_frame {
 
+/*
+Мама мыла раму
+*/
+
+         public static void main(String[] args) {
+            //напишите тут ваш код
+
+        }
 }
