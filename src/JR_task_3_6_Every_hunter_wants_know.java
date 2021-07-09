8c39782705046aad2cf5a39a1040222291d6d8ea
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-09 22:00:01 +0300

    + fix

--- src/JR_task_3_6_Every_hunter_wants_know.java
+++ src/JR_task_3_6_Every_hunter_wants_know.java
@@ -4,25 +4,31 @@ public class JR_task_3_6_Every_hunter_wants_know {
 Каждый охотник желает знать…
 */
 
-         public static void main(String[] args) {
-            Red red = new Red();
-            Orange orange = new Orange();
-            Yellow yellow = new Yellow();
-            Green green = new Green();
-            Blue blue = new Blue();
-            Indigo indigo = new Indigo();
-            Violet violet = new Violet();//напишите тут ваш код
+    public static void main(String[] args) {
+        Red red = new Red();
+        Orange orange = new Orange();
+        Yellow yellow = new Yellow();
+        Green green = new Green();
+        Blue blue = new Blue();
+        Indigo indigo = new Indigo();
+        Violet violet = new Violet();//напишите тут ваш код
 
-        }
+    }
 
-        public static class Red {
-            public Red() {
-                System.out.println("Red");
-            }
+    public static class Red {
+        public Red() {
+            System.out.println("Red");
         }
+    }
 
-        public static class Orange {
-            public Orange() {
-                System.out.println("Orange");
-            }
+    public static class Orange {
+        public Orange() {
+            System.out.println("Orange");
         }
+    }
+    public static class Yellow {
+        public Yellow() {
+            System.out.println("Yellow");
+        }
+    }
+}
