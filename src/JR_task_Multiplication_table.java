a98ffbcfb2adfb2d712efcffd4dc237f3ac768cf
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-09 21:48:35 +0300

    + fix

--- src/JR_task_Multiplication_table.java
+++ src/JR_task_Multiplication_table.java
@@ -4,7 +4,6 @@ public class JR_task_Multiplication_table {
 Таблица умножения Ӏ Java Syntax: 3 уровень, 6 лекция
 */
 
-
         public static void main(String[] args) {
             for(int i = 1; i<11; i++)
                 System.out.print(i+" ");
