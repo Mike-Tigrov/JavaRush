f3c86bcc69776a48e2c8bd4e24f64c472e5a42b8
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-09 21:45:53 +0300

    + task

--- src/JR_task_Multiplication_table.java
+++ src/JR_task_Multiplication_table.java
@@ -1,2 +1,51 @@
 public class JR_task_Multiplication_table {
-}
+
+/*
+Таблица умножения Ӏ Java Syntax: 3 уровень, 6 лекция
+*/
+
+
+        public static void main(String[] args) {
+            for(int i = 1; i<11; i++)
+                System.out.print(i+" ");
+            System.out.println();
+
+            for(int a = 1; a<11; a++)
+                System.out.print(a*2+" ");
+            System.out.println();
+
+            for(int b = 1; b<11; b++)
+                System.out.print(b*3+" ");
+            System.out.println();
+
+            for(int c = 1; c<11; c++)
+                System.out.print(c*4+" ");
+            System.out.println();
+
+            for(int d = 1; d<11; d++)
+                System.out.print(d*5+" ");
+            System.out.println();
+
+            for(int e = 1; e<11; e++)
+                System.out.print(e*6+" ");
+            System.out.println();
+
+            for(int f = 1; f<11; f++)
+                System.out.print(f*7+" ");
+            System.out.println();
+
+            for(int g = 1; g<11; g++)
+                System.out.print(g*8+" ");
+            System.out.println();
+
+            for(int h = 1; h<11; h++)
+                System.out.print(h*9+" ");
+            System.out.println();
+
+            for(int j= 1; j<11; j++)
+                System.out.print(j*10+" ");
+            System.out.println();
+
+        }
+
+    }
