9ea2be1a677828f48ceffe4a3d5ddf96dd4f33aa
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-11 21:19:06 +0300

    + new task

--- src/JR_task_3_6_Escape_characters.java
+++ src/JR_task_3_6_Escape_characters.java
@@ -1,2 +1,15 @@
 public class JR_task_3_6_Escape_characters {
-}
+
+/*
+Экранирование символов
+*/
+
+        public static void main(String[] args) {
+            String a = " It's a Windows path: ";
+            String aa = "\"C:\\Program Files\\Java\\jdk1.7.0\\bin\"";
+            System.out.println(a+aa);
+            String b = "It's a Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk1.7.0\\\\bin\\\"";
+            System.out.println(b);
+
+        }
+    }
