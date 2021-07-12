8f3856c2ee63a8515b366fcb5f04d060b4a7e326
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-12 20:48:31 +0300

    + new task

---
+++ src/JR_task_8_6_Longest_sequence.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JR_task_8_6_Longest_sequence {
+
+/*
+Cамая длинная последовательность
+*/
+
+        public static void main(String[] args) throws IOException {
+            //напишите тут ваш код
+            ArrayList<Integer> nums = new ArrayList<Integer>();
+
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            for (int i = 0; i < 10; i++) {
+                nums.add(i, Integer.parseInt(reader.readLine()));
+            }
+
+
+            int count1 = 0;
+            int count2 = 0;
+            int count3 = 0;
+
+            int index1 = 0;
+            int index2 = 0;
+            int index3 = 0;
+
+            for (int i = 0; i < nums.size() - 1; i++) {
+                if (nums.get(i) == nums.get(i+1)) {
+                    count1++;
+                    index1 = i;
+                }
+            }
+
+            System.out.println(count1);
+            System.out.println(index1);
+            System.out.println();
+
+            for (int i = index1 + 1; i < nums.size() - (1+i); i++) {
+                if (nums.get(i) == nums.get(i+1)) {
+                    count2++;
+                    index2 = i;
+                }
+            }
+
+            System.out.println(count2);
+            System.out.println(index2);
+            System.out.println();
+
+            for (int i = index2 + 1; i < nums.size() - (1+i); i++) {
+                if (nums.get(i) == nums.get(i+1)) {
+                    count3++;
+                    index3 = i;
+                }
+            }
+
+            System.out.println(count3);
+            System.out.println(index3);
+            System.out.println();
+
+
+
+        }
+    }
+
+
+
+
+
+
+
