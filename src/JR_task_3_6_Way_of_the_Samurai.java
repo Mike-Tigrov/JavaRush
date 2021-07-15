05036bc1fbbbc6a515afede37b377104397c4a9d
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-15 22:11:05 +0300

    + new task

--- src/JR_task_3_6_Way_of_the_Samurai.java
+++ src/JR_task_3_6_Way_of_the_Samurai.java
@@ -1,2 +1,10 @@
 public class JR_task_3_6_Way_of_the_Samurai {
-}
+    /*
+Путь самурая
+*/
+
+        public static void main(String[] args) {
+            System.out.println("日本語");
+
+        }
+    }
