eb0bc7a70a5359b2183ee27608ab1776b320bd80
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-17 18:53:55 +0300

    + some fix

--- src/JR_task_3_8_Plan_to_captured_the_world.java
+++ src/JR_task_3_8_Plan_to_captured_the_world.java
@@ -1,2 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class JR_task_3_8_Plan_to_captured_the_world {
-}
+
+/*
+План по захвату мира
+*/
+
+        public static void main(String[] args) throws Exception {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            String age = reader.readLine();
+            String name = reader.readLine();
+            System.out.println(name+" захватит мир через "+age+" лет. Му-ха-ха!");
+        }
+    }
