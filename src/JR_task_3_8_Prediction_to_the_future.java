ac21f0ad78ea3d0fffd4536fcf90e688be92af42
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-18 18:32:21 +0300

    + fix task

--- src/JR_task_3_8_Prediction_to_the_future.java
+++ src/JR_task_3_8_Prediction_to_the_future.java
@@ -1,2 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class JR_task_3_8_Prediction_to_the_future {
-}
+
+/*
+Предсказание на будущее
+*/
+
+        public static void main(String[] args) throws Exception {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            String name = reader.readLine();
+            String in1 = reader.readLine();
+            String in2 = reader.readLine();
+
+            System.out.println(name+" получает "+in1+" через "+in2+" лет/года.");
+        }
+    }
