9c794fd4e3f742a2ca2be6ac2ba2cfa3a6cc398b
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-19 22:23:03 +0300

    + new task

--- src/JR_task_3_8_Modesty_beautifies_programmist.java
+++ src/JR_task_3_8_Modesty_beautifies_programmist.java
@@ -1,2 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class JR_task_3_8_Modesty_beautifies_programmist {
+
+/*
+Скромность украшает программиста
+*/
+
+        public static void main(String[] args) throws Exception {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+String name = reader.readLine();
+System.out.println(name + " зарабатывает $5.000. Ха-ха-ха!");
+        }
 }
