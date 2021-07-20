4718d9f0d802bfd6d21caf203a6e001ecd8f0e41
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-20 19:55:29 +0300

    + some fix

--- src/JR_task_3_8_Big_and_clean.java
+++ src/JR_task_3_8_Big_and_clean.java
@@ -3,8 +3,18 @@ import java.io.InputStreamReader;
 
 public class JR_task_3_8_Big_and_clean {
 
-/*
-Большая и чистая
-*/
+    /*
+ Большая и чистая
+ */
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));//напишите тут ваш код
+
+        String name1 = reader.readLine();
+        String name2 = reader.readLine();
+        String name3 = reader.readLine();
+
+        System.out.println(name1+" + "+name2+" + "+name3+" = Чистая любовь, да-да!");
+    }
 
 }
