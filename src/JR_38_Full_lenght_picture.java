0a7fcf6abf926c14157651fc22cfaaa921cb200f
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-21 22:02:18 +0300

    + some fix

--- src/JR_38_Full_lenght_picture.java
+++ src/JR_38_Full_lenght_picture.java
@@ -3,4 +3,10 @@ public class JR_38_Full_lenght_picture {
     /*
 Полнометражная картина
 */
+    public static void main(String[] args) {
+        System.out.println(getMetreFromCentimetre(243));
+    }
+
+
+    }
 }
