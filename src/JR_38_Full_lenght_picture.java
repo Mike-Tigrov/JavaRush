3ffe9edc90667599878e08939d7e9ffe60fe11dc
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-21 22:03:42 +0300

    + some fix

--- src/JR_38_Full_lenght_picture.java
+++ src/JR_38_Full_lenght_picture.java
@@ -9,7 +9,7 @@ public class JR_38_Full_lenght_picture {
 
     public static int getMetreFromCentimetre(int centimetre) {
         int a = centimetre/100;
-        return a; //напишите тут ваш код      centimetre
+        return a;
     }
 }
 
