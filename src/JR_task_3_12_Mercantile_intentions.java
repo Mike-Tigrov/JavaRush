dd61cd393a4c93a9e65d344607a518a78dec7351
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-22 21:49:55 +0300

    + some fix

--- src/JR_task_3_12_Mercantile_intentions.java
+++ src/JR_task_3_12_Mercantile_intentions.java
@@ -6,8 +6,8 @@ public class JR_task_3_12_Mercantile_intentions {
 
         public static void main(String[] args) {
 
-            for(int i = 0; i < 10; i++){
-                System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+            for(int i = 1; i < 11; i++){
+                System.out.println(i + "." + " Я хочу большую зарплату, и для этого изучаю Java");
             }
         }
     }
