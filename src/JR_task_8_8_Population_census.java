ee4fe8b1b347540613fe4d37a1359d692b4c5b9d
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-07-26 21:52:51 +0300

    + fix

--- src/JR_task_8_8_Population_census.java
+++ src/JR_task_8_8_Population_census.java
@@ -1,2 +1,44 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class JR_task_8_8_Population_census {
+
+/*
+Перепись населения
+*/
+
+        public static Map<String, String> createMap() {
+            //напишите тут ваш код
+            Map <String, String> fio = new HashMap<String, String>();
+
+            fio.put("Петров", "Саша");
+            fio.put("Егоров", "Вадим");
+            fio.put("Шубин", "Толя");
+
+            fio.put("Керо", "Маша");
+            fio.put("Сокол", "Сергей");
+            fio.put("Керо", "Владислав");
+            fio.put("Громов", "Анатолий");
+
+            fio.put("Забелин", "Сергей");
+            fio.put("Керо", "Нина");
+            fio.put("Валов", "Роман");
+
+            return fio;
+        }
+
+        public static int getCountTheSameFirstName(Map<String, String> fio, "Керо") {
+            //напишите тут ваш код
+            for (
+                    return null;
+        }
+
+        public static int getCountTheSameLastName(Map<String, String> map, String lastName) {
+            //напишите тут ваш код
+            return null;
+        }
+
+        public static void main(String[] args) {
+
+        }
 }
