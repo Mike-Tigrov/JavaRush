485542234eed260867d9c9624533fa5fb2fcc043
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-08-02 23:09:22 +0300

    + rename

--- src/JR_task_01_08_Number_squared.java
+++ src/JR_task_01_08_Number_squared.java
@@ -2,7 +2,7 @@
 Число в квадрате
 */
 
-public class JR_task_1_8_Number_squared {
+public class JR_task_01_08_Number_squared {
 
         public static void main(String[] args) {
             int a = sqr(5);
