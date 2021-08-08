ceeb33478f893ace2b48554dd8090917b24d51f9
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-08-08 21:39:44 +0300

    + new task

--- src/JR_task_08_08_Only_for_the_rich.java
+++ src/JR_task_08_08_Only_for_the_rich.java
@@ -1,2 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class JR_task_08_08_Only_for_the_rich {
+
+/*
+Только для богачей
+*/
+
+         public static Map<String, Integer> createMap() {
+            // код
+        }
+
+        public static void removeItemFromMap(Map<String, Integer> map) {
+            // код
+        }
+
+        public static void main(String[] args) {
+
+        }
 }
