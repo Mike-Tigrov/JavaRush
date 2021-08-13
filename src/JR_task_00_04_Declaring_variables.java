b10a4f5b59fe598f06c3b2c0fd225ddb4da42ab8
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-08-13 20:56:35 +0300

    + rename

--- src/JR_task_00_04_Declaring_variables.java
+++ src/JR_task_00_04_Declaring_variables.java
@@ -2,7 +2,7 @@
 Объявляем переменные
 */
 
-public class JR_task_0_04_Declaring_variables {
+public class JR_task_00_04_Declaring_variables {
     public static void main(String[] args) {
         String name;
         String city;
