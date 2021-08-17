c1139b30deb7e00842b1092edac3f139444ec261
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-08-17 20:32:24 +0300

    + some fix

--- src/JR_task_03_12_Financial_expectations.java
+++ src/JR_task_03_12_Financial_expectations.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class JR_task_3_12_Financial_expectations {
+public class JR_task_03_12_Financial_expectations {
 
 /*
 Финансовые ожидания
