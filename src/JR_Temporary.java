4e1255409458c5d6ff7e093fbd9c50c3d2926495
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-08-24 11:43:19 +0300

    Update and rename JR_activiti_temp.java to JR_Temporary.java

--- src/JR_Temporary.java
+++ src/JR_Temporary.java
@@ -1,4 +1,4 @@
-public class JR_activiti {
+public class JR_Temporary {
     public static void main (String[] agrs) {
         byte number = (byte) (1 + Math.random() * 9);
         System.out.print(number);
