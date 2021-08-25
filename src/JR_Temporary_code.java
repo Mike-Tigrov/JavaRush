2bbcdc6340b145010fea1c8ba358382d50388646
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-08-25 23:05:50 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/JR_activiti_temp.java

--- src/JR_Temporary_code.java
+++ src/JR_Temporary_code.java
@@ -2,16 +2,25 @@ public class JR_Temporary_code {
     public static void main (String[] agrs) {
         int number_base = 678;
         int number3 = number_base % 10;
-        int number1 = number3 % 10;
+        int number1 = number_base / 100;
+        int number2 = (number_base / 10) % 10;
 
         System.out.print("Базовое трёхзначное число: ");
         System.out.println(number_base);
 
+        System.out.print("Первая цифра базового числа: ");
+        System.out.println(number1);
+
+        System.out.print("Вторая цифра базового числа: ");
+        System.out.println(number2);
+
         System.out.print("Третья цифра базового числа: ");
         System.out.println(number3);
 
+System.out.print("Сумма цифр базового числа: ");
+System.out.println(number1 + number2 + number3);
 
-        System.out.println(number1);
+
     }
 }
 
