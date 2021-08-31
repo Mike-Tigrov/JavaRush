de87c1b6b7585bbff16d0544542cd687e9870d29
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-08-31 20:53:26 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/JR_activiti_temp.java

--- src/JR_task_4_1_This_age_does_not_suit_to_me.java
+++ src/JR_task_4_1_This_age_does_not_suit_to_me.java
@@ -2,5 +2,7 @@ public class JR_task_4_1_This_age_does_not_suit_to_me {
 /*
 Мне не подходит этот возраст…
 */
-
+public static void main(String[] args) {
+    Person person = new Person();
+    System.out.println("Age is: " + person.age);
 }
