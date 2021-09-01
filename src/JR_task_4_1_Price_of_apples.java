a97cb73b5558d17c47fde276e84a5cd9367d2dce
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-09-01 23:30:54 +0300

    code

--- src/JR_task_4_1_Price_of_apples.java
+++ src/JR_task_4_1_Price_of_apples.java
@@ -3,5 +3,11 @@ public class JR_task_4_1_Price_of_apples {
 /*
 Цена яблок
 */
-
+public static void main(String[] args) {
+    Apple apple = new Apple();
+    apple.addPrice(50);
+    Apple apple2 = new Apple();
+    apple2.addPrice(100);
+    System.out.println("Стоимость яблок " + Apple.applesPrice);
+}
 }
