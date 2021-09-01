e1c40f0f9563fa16fdf5d1ead277167541a4148e
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-09-01 23:34:30 +0300

    some fix

--- src/JR_task_4_1_Price_of_apples.java
+++ src/JR_task_4_1_Price_of_apples.java
@@ -1,20 +1,22 @@
 public class JR_task_4_1_Price_of_apples {
 
-/*
-Цена яблок
-*/
-public static void main(String[] args) {
-    Apple apple = new Apple();
-    apple.addPrice(50);
-    Apple apple2 = new Apple();
-    apple2.addPrice(100);
-    System.out.println("Стоимость яблок " + Apple.applesPrice);
-}
+    /*
+    Цена яблок
+    */
+    public static void main(String[] args) {
+        Apple apple = new Apple();
+        apple.addPrice(50);
+        Apple apple2 = new Apple();
+        apple2.addPrice(100);
+        System.out.println("Стоимость яблок " + Apple.applesPrice);
+    }
+
     public static class Apple {
         public static int applesPrice = 0;
+
         public static void addPrice(int applesPrice) {
-            Apple.applesPrice =  Apple.applesPrice + applesPrice;
+            Apple.applesPrice = Apple.applesPrice + applesPrice;
 
         }
     }
-
+}
