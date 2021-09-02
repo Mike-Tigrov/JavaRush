d97728b30acd60894dcdcba81d9df7565731a605
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-09-02 21:15:43 +0300

    some fix

--- src/JR_task_4_2_How_name_is_cat.java
+++ src/JR_task_4_2_How_name_is_cat.java
@@ -6,11 +6,12 @@ public class Cat {
     private String name = "безымянный кот";
 
     public void setName(String name) {
-
     }
+
     public static void main(String[] args) {
         Cat cat = new Cat();
         cat.setName("Жужик");
+        System.out.println(cat.name);
     }
-}
-}
+}}
+
