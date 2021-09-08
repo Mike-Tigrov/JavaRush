8f2c7abb6a02ff8d1aa41a214db47e450bd12de3
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-09-08 21:09:41 +0300

    some code

--- src/JR_task_04_2_Counting_cats.java
+++ src/JR_task_04_2_Counting_cats.java
@@ -1,10 +1,10 @@
-public class JR_task_4_2_Counting_cats {
+public class JR_task_04_2_Counting_cats {
 
     private static int catsCount = 0;
 
     public static void addNewCat() {
 
-        JR_task_4_2_Counting_cats.catsCount += + 1;
+        JR_task_04_2_Counting_cats.catsCount += + 1;
     }
 
     public static void main(String[] args) {
