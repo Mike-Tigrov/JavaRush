36334631704f3574eb7666ea167bf7a58390dfdf
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-09-08 21:08:07 +0300

    some code

--- src/JR_t_4_2_Counting_cats.java
+++ src/JR_t_4_2_Counting_cats.java
@@ -1,7 +1,13 @@
 public class JR_t_4_2_Counting_cats {
 
-/*
-Учет котов
-*/
+    private static int catsCount = 0;
 
+    public static void addNewCat() {
+
+        JR_t_4_2_Counting_cats.catsCount += + 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
