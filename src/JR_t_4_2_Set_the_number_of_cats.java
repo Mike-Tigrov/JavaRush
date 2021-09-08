c1b064df664001be8820e28f96c639e5bcae975d
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-09-08 21:16:34 +0300

    some fix

--- src/JR_t_4_2_Set_the_number_of_cats.java
+++ src/JR_t_4_2_Set_the_number_of_cats.java
@@ -3,4 +3,14 @@ public class JR_t_4_2_Set_the_number_of_cats {
 Задаем количество котов
 */
 
+    private static int catsCount = 0;
+
+    public static void setCatsCount(int catsCount) {
+        //напишите тут ваш код
+        JR_t_4_2_Set_the_number_of_cats.catsCount += catsCount;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
