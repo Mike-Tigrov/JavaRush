7e3cda0d640a85efc4f08e76847058098fc85d85
Mike-Tigrov <xiwnik@yahoo.com>
2021-09-14 21:03:51 +0300

    +conditions

--- src/JR_task_04_02_Name_tracker.java
+++ src/JR_task_04_02_Name_tracker.java
@@ -2,13 +2,17 @@ public class JR_task_04_02_Name_tracker {
     /*
 Программа учета имен
 
-Условие:
-1.
+Цель:
+1. Напишите код метода setName, чтобы с его помощью установить значение private String fullName
+равное значению локальной переменной String fullName.
 
 Требование:
 1. Класс Cat должен содержать только одну переменную fullName.
 2. Переменная FullName должна иметь модификатор доступа private и тип String.
 3. Класс Cat должен содержать только два метода setName и main.
+4. Метод setName должен иметь локальную переменную fullName.
+5. Метод setName класса Cat должен устанавливать значение
+глобальной переменной private String fullName равным локальной переменной fullName.
 */
     private String fullName;
 
