5652c0357e0564ede90a6d609d298ece2e7def1c
Mike-Tigrov <xiwnik@yahoo.com>
2021-09-15 21:11:02 +0300

    + conditions

--- src/JR_task_00_01_Amigo_very_smart.java
+++ src/JR_task_00_01_Amigo_very_smart.java
@@ -1,6 +1,11 @@
 public class JR_task_00_01_Amigo_very_smart {
 /*
 Амиго очень умный
+
+Задача:
+Написать программу, которая выводит на экран надпись: "Амиго очень умный".
+
+Требования:
 */
 
             public static void main(String[] args) {
