327dc7d3d3f4a956d6b84ce1cf6f0379fde0cd14
Mike-Tigrov <xiwnik@yahoo.com>
2021-09-22 19:25:10 +0300

    some code

--- src/JR_t_8_8.java
+++ src/JR_t_8_8.java
@@ -1,2 +1,83 @@
 public class JR_t_8_8 {
+    package com.javarush.task.task08.task0816;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Iterator;
+
+/*
+Добрая Зинаида и летние каникулы
+*/
+
+    public class Solution {
+        public static Map<String, Date> createMap() throws ParseException {
+            DateFormat dateFormat = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
+            Map<String, Date> name_date = new HashMap<>();
+
+            name_date.put("Смирнов", dateFormat.parse("MAY 1 2012"));
+
+            //напишите тут ваш код
+
+            name_date.put("Сидоров", dateFormat.parse("Jun 4 2003"));
+            name_date.put("Гориванов", dateFormat.parse("APR 16 1984"));
+            name_date.put("Стукалов", dateFormat.parse("SEP 7 1999"));
+
+            name_date.put("Гнойко", dateFormat.parse("NOV 24 1976"));
+            name_date.put("Борисов", dateFormat.parse("DEC 28 2001"));
+            name_date.put("Чекменёв", dateFormat.parse("AUG 10 1987"));
+
+            name_date.put("Сизонов", dateFormat.parse("OCT 22 1984"));
+            name_date.put("Корлаков", dateFormat.parse("JUN 12 1981"));
+            name_date.put("Полянский", dateFormat.parse("SEP 29 2000"));
+
+            return name_date;
+        }
+
+/*
+    public static void removeAllSummerPeople(Map<String, Date> map) {
+        //напишите тут ваш код
+return map;
+    }
+*/
+
+        public static void main(String[] args) {
+            System.out.println(createMap());
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
